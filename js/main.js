d7b0d9a2e551715c0737177331333d00f22c4d7c
tedlife <zhongguojym@gmail.com>
2018-06-22 09:51:55 +0800

    fix sw.js url

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ var markers = [];
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/sw.js")
+      .register("./sw.js")
       .then(function(registration) {
         // Registration was successful
         console.log(
